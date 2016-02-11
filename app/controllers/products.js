6cccd6b28a63784573c1d10c180ac3df7103a937
webgen <webgen@outlook.com>
2016-02-11 21:23:23 +0400

    checkpoint, select box in progress

--- app/controllers/products.js
+++ app/controllers/products.js
@@ -1,11 +1,32 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  names:['lasha','abulashvili'],
   productsBottle: Ember.computed.filterBy('model', 'ptype', 'productsBottle'),
   productsQvevri: Ember.computed.filterBy('model', 'ptype', 'productsQvevri'),
   productsSpirit: Ember.computed.filterBy('model', 'ptype', 'productsSpirit'),
 
+
+  //testArray: [{name: 'lasha'},{name: 'zoro'}],
+  selectedValue: 2,
+
+  actions: {
+    change() {
+      //const changeAction = this.send('someAction');
+      const selectedEl = Ember.$('select')[0];
+      const selectedIndex = selectedEl.selectedIndex;
+      //const testArray = this.get('testArray');
+      const selectedValue = this.get('productsBottle')[selectedIndex];
+      
+      console.log("selectedValue: " + selectedValue.name);
+      console.log(selectedEl[0].name);
+      //Ember.set('selectedValue', selectedValue);
+      //changeAction(selectedValue);
+    },
+    someAction(){
+
+    }
+  },
+  
   breakpoints: [{
           'breakpoint': 1024,
           'settings': {
