c5cebf13f04d5c107c4345924a320b5cde870705
webgen <webgen@outlook.com>
2016-03-03 17:50:59 +0400

    checkpoint, upgraded ember@2.4.1

--- app/app.js
+++ app/app.js
@@ -1,7 +1,8 @@
 import Ember from 'ember';
 import Resolver from './resolver';
-import loadInitializers from 'ember/load-initializers';
+import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
+
 let App;
 
 Ember.MODEL_FACTORY_INJECTIONS = true;
