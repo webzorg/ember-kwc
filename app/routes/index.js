7d82e88175cf3a7d28718373473338fd9ab1e081
webgen <webgen@outlook.com>
2016-05-05 17:26:13 +0400

    minor swiper changes

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,14 +3,18 @@ import Ember from 'ember';
 export default Ember.Route.extend({
 	activate: function(){
 		Ember.run.scheduleOnce('afterRender', this, function() {
-			
 			var mySwiper = new Swiper ('.swiper-container', {
 	    		loop: true,
-	    		speed:1500,
+	    		speed:1000,
     			autoplay:5000,
     			autoplayDisableOnInteraction: false,
     			//pagination: '.swiper-pagination',
-    			grabCursor:true
+    			grabCursor:true,
+    			effect: 'fade',
+				fade: {crossFade: false},
+				//cube: {slideShadows: true, shadow: true, shadowOffset: 20, shadowScale: 0.94},
+				//coverflow: {rotate: 50, stretch: 0, depth: 100, modifier: 1, slideShadows : true},
+				//flip: {slideShadows : true, limitRotation: true}
   			});
   			this.set('mySwiper', mySwiper);
 		});
