d85356bb1f02d002291881890821912ded7b00ce
webgen <webgen@outlook.com>
2016-05-25 11:34:52 +0400

    excluded products from fingerprinting

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 import Ember from 'ember';
 
 export default JSONAPIAdapter.extend({
-	//namespace: 'bla'
+	//namespace: 'api'
 	host: 'http://api.socialatlas.net',
 	pathForType: function(type) {
     	return Ember.String.pluralize(Ember.String.underscore(type));
--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,12 +7,16 @@ module.exports = function(defaults) {
     // Add options here
     'ember-cli-foundation-6-sass': {
       'foundationJs': 'all'
+    },
+    fingerprint: {
+      //generateAssetMap: true,
+      exclude: ['images/products/']
     }
   });
   app.import('bower_components/Swiper/dist/js/swiper.jquery.min.js');
   app.import('bower_components/Swiper/dist/css/swiper.min.css');
   app.import('bower_components/jquery.kwicks/jquery.kwicks.min.js');
-  app.import('bower_components/ihover/src/ihover.css');//minimize it...
+  //app.import('bower_components/ihover/src/ihover.css');//minimize it...
   //app.import('bower_components/photoswipe/dist/photoswipe-ui-default.min.js');
   
   // Use `app.import` to add additional libraries to the generated
