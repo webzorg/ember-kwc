f5d2f707d465340ef63ed9180352d114f0380f54
webgen <webgen@outlook.com>
2016-06-16 23:49:02 +0400

    changed adapter for production

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -4,7 +4,7 @@ import Ember from 'ember';
 export default JSONAPIAdapter.extend({
 	i18n: Ember.inject.service(),
 	namespace: Ember.computed.alias('i18n.locale'),
-	//host: 'http://api.socialatlas.net',
+	host: 'http://api.socialatlas.net',
 	pathForType: function(type) {
 		return Ember.String.pluralize(Ember.String.underscore(type));
   	}
