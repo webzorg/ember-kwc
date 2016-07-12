39b0b6bfa17d5b5c0a9a24b37ee34bf3d5d2c764
webgen <webgen@outlook.com>
2016-07-12 17:48:58 +0400

    added model based index/slideshow. refined use of swiper.

--- app/components/index-swiper.js
+++ app/components/index-swiper.js
@@ -2,26 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	didInsertElement: function(){
-
-		Ember.$('.swiper-wrapper').children('div').each(function (index) {
-		    Ember.$(this).css('background-image', 'url("/assets/images/slideshow/'+index+'.jpg")');
-		});
-		Ember.$('.swiper-wrapper').hide();
 		
 		var swiperLoad = imagesLoaded(Ember.$(".swiper-wrapper"));
 		swiperLoad.on('done', ()=> {				
-			Ember.$('.swiper-wrapper').fadeIn(1600);
-			Ember.$('#loader-gif').remove();
 
 			var mySwiper = new Swiper ('.swiper-container', {
 	    		loop: true,
 	    		speed:1000,
-				autoplay:5000,
+				autoplay:10000,
 				autoplayDisableOnInteraction: false,
 				pagination: '.swiper-pagination',
 				paginationType:'bullets',
 				paginationClickable:true,
-				//grabCursor:true,
+				lazyLoading:true,
+				lazyLoadingInPrevNext:true,
+				grabCursor:true,
 				effect: 'fade',
 				fade: {crossFade: false}
 				//cube: {slideShadows: true, shadow: true, shadowOffset: 20, shadowScale: 0.94},
